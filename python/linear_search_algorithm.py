3901f18cc1e1edcfe65401ec33909bc4b17c7f70
Dejanrobi <dejanrobi93@gmail.com>
2023-01-11 20:40:52 +0300

    Linear search algorithm
    This is the first algorithm to be committed: the linear search algorithm

---
+++ python/linear_search_algorithm.py
@@ -0,0 +1,30 @@
+# This is a linear search Algorithm
+# 1. The algorithm start at the start of a list
+# 2. Compares the current value with the target value
+# 3. If the current value is equal to the target value, the index of the value is returned
+# 4. If not, it moves to the next value sequentially and and repeats step 2
+
+def linearSearch(arr, target):
+  #     looping through the array
+    for i in range(0, len(arr)):
+#         checking whether the current value is equal to the target
+#         print(arr[i])
+        if(arr[i]==target):
+#             print(arr[i])
+            return i
+            break
+        
+    return -1
+        
+        
+arr =[3,4,5,6,8,8,9,10]
+# print(linearSearch(arr,6))
+result = linearSearch(arr,9)
+
+verify(result)
+# A function to verify
+def verify(index):
+    if(index==-1):
+        print("The value was not found in the list")
+    else:
+        print("The value was found at index: ", index)
